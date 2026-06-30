12e913928f36e19dad21b255da622bdaa26d94ec
frhn-06 <farhanmunif233@gmail.com>
2026-06-30 22:59:47 +0700

    debugging swagger

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,6 +1,9 @@
 import { Express } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger";
+import fs from "fs";
+
+console.log(fs.existsSync("./src/routes/api.ts"));
 
 const docs = (app: Express) => {
     console.log(swaggerSpec);
